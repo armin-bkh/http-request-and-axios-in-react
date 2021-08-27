ce490eba4707750c913e7e050c541495a2316da1
armin bakhshi <arminbkh0921@yahoo.com>
2021-08-27 20:22:31 +0430

    handle error when get data

--- src/Components/CommentList/CommentList.jsx
+++ src/Components/CommentList/CommentList.jsx
@@ -1,16 +1,35 @@
 import Comment from "../Comment/Comment";
 
-const CommentList = ({ comments, onClick }) => {
-  return comments.length ? (
-    <section className={`d-flex-row p-5`}>
-      {comments.map((comnt) => (
-        <Comment key={comnt.id} name={comnt.name} email={comnt.email} onClick={()=> onClick(comnt.id)} />
-      ))}
-    </section>
-  ) :
-  <section className={`h-60 d-flex-row justify-center`}>
-    <h1 className={`text-center`}>loading...</h1>
-  </section> 
+const CommentList = ({ comments, onClick, error }) => {
+  // return comments.length ? (
+  //   <section className={`d-flex-row p-5`}>
+  //     {comments.map((comnt) => (
+  //       <Comment key={comnt.id} name={comnt.name} email={comnt.email} onClick={()=> onClick(comnt.id)} />
+  //     ))}
+  //   </section>
+  // ) :
+  // <section className={`h-60 d-flex-row justify-center`}>
+  //   <h1 className={`text-center`}>loading...</h1>
+  // </section> 
+
+
+  const renderComments = () => {
+    let renderValue = <h1 className={`text-2xl text-center`}>Loading...</h1>;
+
+    if(error) renderValue = <h1 className={`text-2xl text-center`}>{error.message}</h1>;
+
+
+    if(comments.length && !error){
+      renderValue = comments.map((comnt) => (
+              <Comment key={comnt.id} name={comnt.name} email={comnt.email} onClick={()=> onClick(comnt.id)} />
+            ))
+    }
+
+    return renderValue;
+  }
+
+
+  return <section className={`d-flex-row p-5 ${!comments.length && `justify-center`}`}>{renderComments()}</section>
 };
 
 export default CommentList;
--- src/Container/Discusstion/Discussion.jsx
+++ src/Container/Discusstion/Discussion.jsx
@@ -7,6 +7,8 @@ import NewComment from "../../Components/NewComment/NewComment"
 const Discusstion = () => {
     const [comments, setComments] = useState([]);
     const [commentId, setCommentId] = useState(null);
+    const [error, setError] = useState(false);
+
     useEffect(()=>{
         const getComments = async () => {
             try{
@@ -14,7 +16,7 @@ const Discusstion = () => {
                 setComments(data.splice(0, 4));
             }
             catch(err){
-                console.log(err);
+                setError({message: "fetching data failed"});
             }
         }
         getComments();
@@ -26,7 +28,7 @@ const Discusstion = () => {
 
     return ( 
         <>
-            <CommentList comments={comments} onClick={selectCommentHandler} />
+            <CommentList error={error} comments={comments} onClick={selectCommentHandler} />
             <FullComment commentId={commentId} />
             <NewComment />
         </>
