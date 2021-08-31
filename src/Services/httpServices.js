b983d50e4069493427d2de243a02d1ac1923985a
armin bakhshi <arminbkh0921@yahoo.com>
2021-08-31 20:35:09 +0430

    add interceptor to the axios setting

--- src/Services/httpServices.js
+++ src/Services/httpServices.js
@@ -2,11 +2,24 @@ import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:3001";
 
+axios.interceptors.response.use(
+  (response) => {
+      console.log(response);
+
+      return response
+  },
+  (err) => {
+      console.log(err);
+
+      return Promise.reject();
+  }
+);
+
 const http = {
-    get: axios.get,
-    put: axios.put,
-    post: axios.post,
-    delete: axios.delete,
-}
+  get: axios.get,
+  put: axios.put,
+  post: axios.post,
+  delete: axios.delete,
+};
 
 export default http;
