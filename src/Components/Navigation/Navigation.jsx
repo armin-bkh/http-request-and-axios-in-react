7ec306730dd5df4337dcdd79da720cabde97bb98
armin bakhshi <arminbkh0921@yahoo.com>
2021-09-03 17:43:01 +0430

    Navigation component create

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,11 +1,22 @@
+import { Link, withRouter } from "react-router-dom";
+
+const links = [
+  { title: "Home", to: "/" },
+  { title: "New comment", to: "/new-comment" },
+];
+
 const Navigation = () => {
-    return ( 
-        <nav>
-            {
-                links.map(link => <)
-            }
-        </nav>
-     );
-}
- 
-export default Navigation;
+  return (
+    <nav>
+      <ul>
+        {links.map((link) => (
+          <li key={link.to}>
+            <Link to={link.to}>{link.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default withRouter(Navigation);
